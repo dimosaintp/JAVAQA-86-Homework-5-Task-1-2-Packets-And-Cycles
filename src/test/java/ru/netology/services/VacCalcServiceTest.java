0fefce6062b57789ee7351e86cd35cb071fbdf89
Dmitrii Odynets <158339965+dimosaintp@users.noreply.github.com>
2024-05-08 23:40:41 +0300

    Added ParameterizedTest

--- src/test/java/ru/netology/services/VacCalcServiceTest.java
+++ src/test/java/ru/netology/services/VacCalcServiceTest.java
@@ -2,8 +2,25 @@ package ru.netology.services;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 public class VacCalcServiceTest {
+
+    @ParameterizedTest
+    @CsvSource({
+            "3, 10000, 3000, 20000",
+            "2, 100000, 60000, 150000"
+    })
+    public void examples(int expected, int income, int expenses, int threshold) {
+        VacCalcService service = new VacCalcService();
+
+        int actual = service.calculate(income, expenses, threshold);
+        Assertions.assertEquals(expected, actual);
+
+    }
+
+    /*
 
     @Test
     public void example1(){
@@ -22,4 +39,7 @@ public class VacCalcServiceTest {
         int actual = service.calculate(100_000, 60_000, 150_000);
         Assertions.assertEquals(expected, actual);
     }
+
+     */
+
 }
