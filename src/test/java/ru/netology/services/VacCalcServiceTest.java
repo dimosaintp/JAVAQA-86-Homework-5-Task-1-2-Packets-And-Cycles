752144589349c48e8f43ce9c25e13bf701ba8429
Dmitrii Odynets <158339965+dimosaintp@users.noreply.github.com>
2024-05-08 23:59:24 +0300

    Added ParameterizedTest from csv file

--- src/test/java/ru/netology/services/VacCalcServiceTest.java
+++ src/test/java/ru/netology/services/VacCalcServiceTest.java
@@ -3,15 +3,21 @@ package ru.netology.services;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 import org.junit.jupiter.params.provider.CsvSource;
 
 public class VacCalcServiceTest {
 
     @ParameterizedTest
+    @CsvFileSource(files = "src/test/resources/examples.csv")
+    /*
+
     @CsvSource({
             "3, 10000, 3000, 20000",
             "2, 100000, 60000, 150000"
     })
+
+     */
     public void examples(int expected, int income, int expenses, int threshold) {
         VacCalcService service = new VacCalcService();
 
